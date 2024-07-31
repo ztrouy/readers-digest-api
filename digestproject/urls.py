67a23fdb2ce6827879a07197f487927828321442
Zachary Trouy <ztrouy@gmail.com>
2024-07-31 14:02:36 -0500

    Added routing for Categories

--- digestproject/urls.py
+++ digestproject/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from digestapi.views import UserViewSet
+from digestapi.views import UserViewSet, CategoryViewSet
 
 router = DefaultRouter(trailing_slash=False)
+
+router.register(r"categories", CategoryViewSet, "category")
 
 urlpatterns = [
     path("", include(router.urls)),
