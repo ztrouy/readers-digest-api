3d169b1dc7b09c57059acd94639df1815a5bea64
Zachary Trouy <ztrouy@gmail.com>
2024-08-01 11:56:04 -0500

    Added routing for Books

--- digestproject/urls.py
+++ digestproject/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from digestapi.views import UserViewSet, CategoryViewSet
+from digestapi.views import UserViewSet, CategoryViewSet, BookViewSet
 
 router = DefaultRouter(trailing_slash=False)
 
 router.register(r"categories", CategoryViewSet, "category")
+router.register(r"books", BookViewSet, "book")
 
 urlpatterns = [
     path("", include(router.urls)),
